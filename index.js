059cffed9a5f75fbbab7706b975437e3a98bde05
Haneen-Abdulgllil <haneenabdulglil2018@yahoo.com>
2023-03-17 10:59:27 +0300

    assignment complated

--- index.js
+++ index.js
@@ -12,5 +12,9 @@ const tutorials = [
 ];
 
 const titleCased = () => {
-  return tutorials
+  return tutorials.map((item) => {
+    return item.split(' ').map( (word) => {
+      return word.charAt(0).toUpperCase() + word.slice(1);
+    }).join(' ');
+  });
 }
